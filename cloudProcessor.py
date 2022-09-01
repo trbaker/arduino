47520ac54dffb63110e350b0296b3324961b9530
Tom R Baker <trbaker@gmail.com>
2022-09-01 11:03:55 -0500

    Update cloudProcessor.py

--- cloudProcessor.py
+++ cloudProcessor.py
@@ -1,9 +1,16 @@
 # Much of the code used in the cloud to process data from Arduino (before sending to AGO) is provided below.
-
+# The script receives data from Arduino, processes it, and sends to ArcGIS Online as a POST request.
+# We edit the default dictionary (treating as a string) and then use the ast library to convert to a dictionary before sending to AGO
 
 import urllib.parse
 import requests
-import json
+import ast
+
+controller="null"
+lat=0
+lon=0
+temperature=0
+humidity=0
 
 def create(request):
   try:
@@ -11,16 +18,28 @@ def create(request):
     temperature = urllib.parse.unquote(request.args.get('temperature'))
     humidity = urllib.parse.unquote(request.args.get('humidity'))
     lat = urllib.parse.unquote(request.args.get('lat'))
-    long = urllib.parse.unquote(request.args.get('long'))
-    payload= "Recevied data. Controller info: " + controller
+    lon = urllib.parse.unquote(request.args.get('lon'))
+    payload= "Received data. Controller info: " + controller
   except:
     payload="Error receiving data from Arduino"
     return payload
   try:
     url = 'https://services3.arcgis.com/GzteEaZqBuJ6GIYr/ArcGIS/rest/services/survey123_910b6ea1c50743269a5b171a91fe6cc7_fieldworker/FeatureServer/0/addFeatures'
-    params = json.dumps({'f':'pjson','token':'','rollbackOnFailure':'false','features':{'geometry':{'x': lat,'y':long},'attributes':{'controller':controller,'temperature':temperature,'humidity':humidity }}})
-    x = requests.post(url, params=params)
-    payload = payload + " AGO response: " + x.text
+    
+    params = {'f': 'html', 'token': '', 'rollbackOnFailure': 'false', 'features':'{ "attributes" : {"controller" : "controller999","temperature" : temperature2,"humidity" : humidity2},"geometry" : {"x" : lon2,"y" : lat2}}'}
+
+    params=str(params).replace("temperature2", str(temperature))
+    params=str(params).replace("humidity2", str(humidity))
+    params=str(params).replace("lat2", str(lat))
+    params=str(params).replace("lon2", str(lon))
+    params=str(params).replace("controller999", str(controller))
+
+    # converts the string to a python dictionary
+    params=ast.literal_eval(params)
+    #submits the dictionary to the URL
+    x=requests.post(url, params)
+    
+    payload = payload + '  AGO Status response:' + str(x.status_code)
   except :
     payload="Error submitting data to ArcGIS Online."
   #output to screen - for testing purposes mostly
